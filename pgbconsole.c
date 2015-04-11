95ca507a0e0a0a94af1828ecfdf7f646cfe27a63
Alexey Lesovsky <lesovsky@gmail.com>
2015-04-11 11:47:10 +0500

    add create_pgbrc_conn

--- pgbconsole.c
+++ pgbconsole.c
@@ -1,14 +1,9 @@
 #include "pgbconsole.h"
 
-struct conn_opts create_initial_conn(int argc, char *argv[], struct conn_opts connections[])
+void create_initial_conn(int argc, char *argv[], struct conn_opts connections[])
 {
     int param, option_index;
     pw = getpwuid(getuid());     // get current user info: pw_name,pw_uid,pw_gid,pw_dir,pw_shell.
-
-    connections[0].hostaddr = "";
-    connections[0].port = "";
-    connections[0].user = "";
-    connections[0].dbname = "";
 
     if (argc > 1)
     {
@@ -29,72 +24,95 @@ struct conn_opts create_initial_conn(int argc, char *argv[], struct conn_opts co
         switch(param)
         {
             case 'h': 
-                connections[0].hostaddr = optarg;
+                strcpy(connections[0].hostaddr, optarg);
                 break;
             case 'p':
-                connections[0].port = optarg;
+                strcpy(connections[0].port, optarg);
                 break;
             case 'U':
-                connections[0].user = optarg;
+                strcpy(connections[0].user, optarg);
                 break;
             case 'd':
-                connections[0].dbname = optarg;
+                strcpy(connections[0].dbname, optarg);
                 break;
-            case 'H':
+            case '?':
                 printf("invoke help printing function here\n", optarg);
                 exit (EXIT_SUCCESS);
                 break;
-            case '?': default: 
+            default: 
                 printf("unknown option.\n");
+                exit(EXIT_FAILURE);
                 break;
         }
     }
 
     while (argc - optind >= 1)
     {
-        if ( (argc - optind > 1) && connections[0].user == "" && connections[0].dbname == "" )
-            connections[0].user = argv[optind];
-        else if ( (argc - optind >= 1) && connections[0].dbname == "" )
-            connections[0].dbname = argv[optind];
+        if ( (argc - optind > 1) && strlen(connections[0].user) == 0 && strlen(connections[0].dbname) == 0 )
+            strcpy(connections[0].user, argv[optind]);
+        else if ( (argc - optind >= 1) && strlen(connections[0].dbname) == 0 )
+            strcpy(connections[0].dbname, argv[optind]);
         else
             fprintf(stderr, "%s: warning: extra command-line argument \"%s\" ignored\n", argv[0], argv[optind]);
 
         optind++;
     }    
 
-    if ( connections[0].hostaddr == "" )
-        connections[0].hostaddr = DEFAULT_HOSTADDR;
+    if ( strlen(connections[0].hostaddr) == 0 )
+        strcpy(connections[0].hostaddr, DEFAULT_HOSTADDR);
 
-    if ( connections[0].port == "" )
-        connections[0].port = DEFAULT_PORT;
+    if ( strlen(connections[0].port) == 0 )
+        strcpy(connections[0].port, DEFAULT_PORT);
 
-    if ( connections[0].user == "" ) {
-        connections[0].user = pw->pw_name;
+    if ( strlen(connections[0].user) == 0 ) {
+        strcpy(connections[0].user, pw->pw_name);
     }
 
-    if ( connections[0].dbname == "" && connections[0].user == "" ) {
-        connections[0].user = pw->pw_name;
-        connections[0].dbname = DEFAULT_DBNAME;
+    if ( strlen(connections[0].dbname) == 0 && strlen(connections[0].user) == 0 ) {
+        strcpy(connections[0].user, pw->pw_name);
+        strcpy(connections[0].dbname, DEFAULT_DBNAME);
     }
-    else if ( connections[0].user != "" && connections[0].dbname == "" )
-        connections[0].dbname = connections[0].user;
+    else if ( strlen(connections[0].user) != 0 && strlen(connections[0].dbname) == 0 )
+        strcpy(connections[0].dbname, connections[0].user);
+
+    connections[0].conn_used = true;
 }
 
-int check_pgbrc(void)
+int create_pgbrc_conn(int argc, char *argv[], struct conn_opts connections[], const int pos)
 {
-    struct stat statbuf;
+    FILE *fp;
+    char strbuf[4096], a[4096], b[4096], c[4096], d[4096];
+    int i = pos;
     pw = getpwuid(getuid());
 
     strcpy(pgbrcpath, pw->pw_dir);
     strcat(pgbrcpath, "/");
     strcat(pgbrcpath, PGBRC_FILE);
 
-    if (stat(pgbrcpath, &statbuf) == -1) 
-        return(PGBRC_NOT_EXIST);
-    else if (statbuf.st_mode & ( S_IRWXG | S_IRWXO) )
-        return(PGBRC_WRONG);
-    else
-        return(PGBRC_EXIST);
+    if ((fp = fopen(pgbrcpath, "r")) != NULL) {
+        while (fgets(strbuf, 4096, fp) != 0) {
+            sscanf(strbuf, "%[^:]:%[^:]:%[^:]:%[^:\n]", a, b, c, d);
+            connections[i].terminal = i;
+            connections[i].conn_used = true;
+            strcpy(connections[i].hostaddr, a);
+            strcpy(connections[i].port, b);
+            strcpy(connections[i].user, c);
+            strcpy(connections[i].dbname, d);
+            i++;
+        }
+        return PGBRC_READ_OK;
+    } else {
+        printf("WARNING: failed to open %s. try use defaults\n", pgbrcpath);
+        return PGBRC_READ_ERR;
+    }
+}
+
+void print_conn(struct conn_opts connections[])
+{
+    int i;
+    for ( i = 0; i < MAX_CONSOLE; i++ )
+        if (connections[i].conn_used)
+            printf("qq: %i %s %s %s %s\n", connections[i].terminal, connections[i].hostaddr, connections[i].port, connections[i].user, connections[i].dbname);
 }
 
 int main (int argc, char *argv[])
@@ -109,24 +127,12 @@ int main (int argc, char *argv[])
  */
     if ( argc > 1 ) {                                           /* input parameters specified */
         create_initial_conn(argc, argv, connections);           /* save input parameters */
-        if (check_pgbrc() == PGBRC_EXIST)
-            printf("parameters specified, use them and try to read pgbrc here\n");                        /* if .pgbrc exist read them too */
-    } else {                                                    /* input parameters not specified */
-        switch (check_pgbrc()) {                                /* check .pgbrc */
-        case PGBRC_WRONG:                                       /* .pgbrc has wrong permissions */
-            printf("WARNING: .pgbrc has wrong permissions.\n");
-            create_initial_conn(argc, argv, connections);       /* try use defaults for connection */
-            break;
-        case PGBRC_NOT_EXIST:                                   /* .pgbrc does not exist */
-            create_initial_conn(argc, argv, connections);       /* try use defaults for connection */
-            break;
-        case PGBRC_EXIST: default:                              /* .pgbrc exist */
-            printf("parameters not specidied, try read pgbrc here\n");                        /* try read him */
-            break; 
-        }
-    }
+        create_pgbrc_conn(argc, argv, connections, 1);
+    } else                                                     /* input parameters not specified */
+        if (create_pgbrc_conn(argc, argv, connections, 0) == PGBRC_READ_ERR)
+            create_initial_conn(argc, argv, connections);
 
-    printf("%s %s %s %s\n", connections[0].hostaddr, connections[0].port, connections[0].user, connections[0].dbname);
+    print_conn(connections);
 
     return 0;
 }
--- pgbconsole.h
+++ pgbconsole.h
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include <limits.h>
 #include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +16,8 @@
 #define DEFAULT_DBNAME "postgres"
 
 #define PGBRC_FILE ".pgbrc"
-#define PGBRC_EXIST 0
-#define PGBRC_NOT_EXIST 1
-#define PGBRC_WRONG 2
+#define PGBRC_READ_OK 0
+#define PGBRC_READ_ERR 1
 #define MAX_CONSOLE 8
 
 static char pgbrcpath[PATH_MAX];
@@ -27,6 +27,7 @@ const char * short_options = "h:p:U:d:";
 
 /* структура содержащая длинные параметры и их короткие аналоги */
 const struct option long_options[] = {
+    {"help", optional_argument, NULL, '?'},
     {"host", required_argument, NULL, 'h'},
     {"port", required_argument, NULL, 'p'},
     {"dbname", required_argument, NULL, 'd'},
@@ -40,19 +41,30 @@ const struct option long_options[] = {
  */
 struct conn_opts
 {
-    char *hostaddr;
-    char *port;
-    char *user;
-    char *dbname;
-    char *password;
+    int terminal;
+    bool conn_used;
+    char hostaddr[4096];
+    char port[4096];
+    char user[4096];
+    char dbname[4096];
+    char password[4096];
 };
 
 /* массив структур который содержит параметры коннектов для всех консолей */
-struct conn_opts connections[MAX_CONSOLE];
+struct conn_opts connections[MAX_CONSOLE] = {
+    { 0, false, "", "", "", "", "" },
+    { 1, false, "", "", "", "", "" },
+    { 2, false, "", "", "", "", "" },
+    { 3, false, "", "", "", "", "" },
+    { 4, false, "", "", "", "", "" },
+    { 5, false, "", "", "", "", "" },
+    { 6, false, "", "", "", "", "" },
+    { 7, false, "", "", "", "", "" }};
 
 /* Structures declaration. */
 static struct passwd *pw;                  // get current user info: pw_name,pw_uid,pw_gid,pw_dir,pw_shell.
 
 /* Functions prototypes */
-struct conn_opts create_initial_conn(int argc, char *argv[], struct conn_opts connections[]);     // обработка входных параметров и установка дефолтов
-int check_pgbrc(void);
+void create_initial_conn(int argc, char *argv[], struct conn_opts connections[]);     // обработка входных параметров и установка дефолтов
+int create_pgbrc_conn(int argc, char *argv[], struct conn_opts connections[], const int pos);
+void print_conn(struct conn_opts connections[]);
